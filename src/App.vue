32b6250aeb4ec8f57818c9c7e3e7060e7941f1dc
Rolland_Dev <rolland.h.dev@gmail.com>
2025-06-12 12:35:15 +0200

    commit in progress Quiz

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,42 @@
-<script setup></script>
+<script setup>
+import { onMounted, ref } from "vue";
+import Quiz from "./components/Quiz.vue";
 
-<template></template>
+const quiz = ref(null);
+const state = ref("loading");
 
-<style scoped></style>
+onMounted(() => {
+  fetch("/quiz.json")
+    .then((r) => {
+      if (r.ok) {
+        return r.json();
+      }
+      throw new Error("Impossible de charger le JSON");
+    })
+    .then((data) => {
+      quiz.value = data;
+      state.value = "idle";
+    })
+    .catch((e) => {
+      state.value = "error";
+    });
+});
+</script>
+
+<template>
+  <h1>Quiz Vue.js</h1>
+  <div v-if="state === 'error'">
+    <p>Impossible de charger le quiz</p>
+  </div>
+
+  <div :aria-busy="state === 'loading'">
+    <Quiz :quiz="quiz" v-if="quiz" />
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  text-align: center;
+  margin: 2rem;
+}
+</style>
---
+++ src/components/Quiz.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+  quiz: {
+    quiz: Object,
+  },
+});
+</script>
+
+<template>
+  <div>
+    <h2>
+      {{ quiz.title }}
+    </h2>
+  </div>
+</template>
+
+<style scoped>
+h2 {
+  text-align: center;
+  margin-bottom: 2rem;
+}
+</style>
